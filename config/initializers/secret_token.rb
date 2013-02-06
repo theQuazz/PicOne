0f0d1e903eef5a26a3011a0909ed12076acd1708
Quazz <djrquazz@gmail.com>
2013-02-05 23:16:42 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twocents::Application.config.secret_token = '21355c0f20b3306f864a508da0f75b34c52e64ef3d1159467f832249f28367dfeec81d07defd3f4009a617a5867d8a4474db8b36c3d89f4b5ea462eabe30231b'
