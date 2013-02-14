924fdc54e93bc3be5fafef3c16fedadeaf688394
Quazz <djrquazz@gmail.com>
2013-02-13 20:11:07 -0500

    Migrated password_hash -> password_digest

---
+++ db/migrate/20130214010337_rename_password_hash_to_password_digest_on_users.rb
@@ -0,0 +1,8 @@
+class RenamePasswordHashToPasswordDigestOnUsers < ActiveRecord::Migration
+  def up
+    rename_column :users, :password_hash, :password_digest
+  end
+
+  def down
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130206042239) do
+ActiveRecord::Schema.define(:version => 20130214010337) do
 
   create_table "collections", :force => true do |t|
     t.integer  "user_id"
@@ -38,12 +38,12 @@ ActiveRecord::Schema.define(:version => 20130206042239) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "nickname"
-    t.string   "password_hash"
+    t.string   "password_digest"
     t.string   "email"
     t.date     "birthday"
     t.string   "gender"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
   end
 
 end
